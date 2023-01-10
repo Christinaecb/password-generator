be60c507479726d4038e5d44c52d3bfd189ae04e
Your Name <you@example.com>
2023-01-10 15:30:38 -0500

    Added the password criteria

--- assets/script.js
+++ assets/script.js
@@ -7,12 +7,23 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Click! ")
 
+// Prompt the user for the password criteria
+// Password length 8 < 128
+// Lowercase, uppercase, numbers, special characters
+var charset = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+     length = 8,
+     retVal = ""
+// Validate the input
+
+// Display password on page
+for (var i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n));
+}
 
 
 
-  
 // The value being returned in password generator box
-return "Generated Password"
+return retVal
   
 }
 

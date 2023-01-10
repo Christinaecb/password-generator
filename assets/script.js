a0e13d184a0ab59e2c7ce2b513834c8ac316c199
Your Name <you@example.com>
2023-01-10 17:05:46 -0500

    Started over, I'm confusing myself with many tutorials

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,32 +15,13 @@ function writePassword() {
 
 }
 
-// Added function to confirm button is being clicked
 function generatePassword() {
-  console.log("Click! ")
 
-// Prompt the user for the password criteria
-// Password length 8 < 128
-// Lowercase, uppercase, numbers, special characters
-var charset = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var length = 8;
-var retVal = "";
-
-for (var i = 0, n = charset.length; i < length; ++i) {
-  retVal += charset.charAt(Math.floor(Math.random() * n));
-// Validate the input
-}
-
-// Display password on page
-
-
-// The value being returned in password generator box
-return retVal;
   
 }
 
 
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+

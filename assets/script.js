40ab026cca05cb106f520795be562a36158d0141
Your Name <you@example.com>
2023-01-10 17:18:31 -0500

    added array for special characters, lower/uppercase characters and numbers

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
 // Assignment code here
+var characterLength = 8;
+var choiceArr = [];
+
+// Array for special characters, upper and lower case characters and numbers
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','_','-','=','+',];
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0',];
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,11 +27,12 @@ function writePassword() {
 
 function generatePassword() {
 
-  
 }
 
-
-
+// Where I get information from
+function getPrompts(){
+  
+}
 
 
 
